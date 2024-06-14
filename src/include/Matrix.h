8eb3acd557cbc2cdc15dbfa5a6b422614c4298ff
AbduKT <abdelra7man87@gmail.com>
2024-06-14 21:05:30 +0200

    Add variant for diffusivity matrix

--- src/include/Matrix.h
+++ src/include/Matrix.h
@@ -72,7 +72,13 @@ using T_DMatx = std::variant<Matd2x2, Matd3x3, Matd6x6>;
  * @brief Variants `elStiffMatx`. It is a pointer because we need this to avoid copy in `Elements::getElStiffMatx`.
  * 
  */
-using T_ElStiffMatx = std::variant<vector<Matd8x8>*, vector<Matd6x6>*, vector<Matd24x24>*>;
+using T_ElStiffMatx = std::variant<vector<Matd4x4>*, vector<Matd8x8>*, vector<Matd6x6>*, vector<Matd24x24>*>;
+
+/**
+ * @brief Variants `elKdMatxMatx`.
+ * 
+ */
+using T_ElKdMatx = std::variant<vector<Matd4x4>*>;
 
 /**
  * @brief Variants `nodStres`.
