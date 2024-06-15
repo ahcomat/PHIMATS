ae79b2407f35d20191b6fc96bd9146e64379b2b3
AbduKT <abdelra7man87@gmail.com>
2024-06-15 11:22:10 +0200

    Update documentation

--- src/include/Materials/Transport/HeatMassTransport.h
+++ src/include/Materials/Transport/HeatMassTransport.h
@@ -27,17 +27,24 @@ public:
 HeatMassTransport(string dimensions, H5IO &H5File, int iSet, string isoType="Isotropic");
 
 /**
- * @brief Returns the 3D stiffness matrix in Voigt notation.
+ * @brief Returns the diffusivity (conductivity) matrix.
  * 
- * @return T_KMatx 
+ * @return T_DMatx 
  */
 T_DMatx getKMatx() const override;
+
+/**
+ * @brief Returns the diffusive (heat) capacity matrix.
+ * 
+ * @return T_DMatx 
+ */
+T_DMatx getKMatx() const;
 
 private:
 
 double rho;         /// @brief Mass density 
 double c;           /// @brief Specific heat (diffusive) capacity
-T_DMatx KMatx;      /// @brief Variant for storing the conductivity matrix
+T_DMatx KMatx;      /// @brief Variant for storing the diffusivity (conductivity) matrix
 
 };
 #endif
