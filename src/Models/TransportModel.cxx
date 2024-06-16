95c06b667f5cc848ef0a049a896fcbd51fa1c6fe
AbduKT <abdelra7man87@gmail.com>
2024-06-16 13:37:41 +0200

    Initial commit

---
+++ src/Models/TransportModel.cxx
@@ -0,0 +1,355 @@
+#include<iostream>
+#include<variant>
+
+#include"Models/TransportModel.h"
+#include "Materials/Transport/HeatMassTransport.h"
+
+using namespace std;
+
+TransportModel::TransportModel(vector<BaseElemTrans*> elements, H5IO& H5File_in){
+
+    string dsetName;
+    dsetName = "SimulationParameters/nSteps";
+    nSteps = H5File_in.ReadScalar(dsetName);
+    dsetName = "SimulationParameters/nDim";
+    nDim = H5File_in.ReadScalar(dsetName);
+    dsetName = "SimulationParameters/nTotElements";
+    nTotElements = H5File_in.ReadScalar(dsetName);
+    dsetName = "SimulationParameters/nElementSets";
+    nElementSets = H5File_in.ReadScalar(dsetName);
+    dsetName = "SimulationParameters/nTotDofs";
+    nTotDofs = H5File_in.ReadScalar(dsetName);
+    dsetName = "SimulationParameters/nTotNodes";
+    nTotNodes = H5File_in.ReadScalar(dsetName);
+    dsetName = "SimulationParameters/dt";
+    dt = H5File_in.ReadScalar(dsetName);
+
+    // Set the type and size
+    nodCount.resize(nTotNodes);
+    if (nDim == 2){ // Case 2D model
+
+        nodFlux = vector<ColVecd2>(nTotNodes);
+
+    } else if (nDim == 3){ // Case 3D
+
+        nodFlux = vector<ColVecd3>(nTotNodes);
+
+    }
+
+    // Initialize to zeros
+    setZero_nodFlux();
+
+    // Allocate memory for `Fint`.
+    PetscMalloc1(nTotDofs, &Fint);
+    // Initialize to zeros, otherwise will get garbage memory values.
+    for(int iDof=0; iDof<nTotDofs; iDof++){
+        Fint[iDof] = 0;
+    }
+
+    InitializePETSc(elements);
+}
+
+TransportModel::~TransportModel(){
+
+    // Deallocate memory.
+    PetscFree(presDofs); PetscFree(presVals); PetscFree(Fint);
+    VecDestroy(&F); VecDestroy(&x); MatDestroy(&K);
+    // Finalize PETSc
+    PetscFinalize();
+    // Exit message
+    cout << "TransportModel model exited correctly" << "\n";
+}
+
+void TransportModel::setZero_nodFlux(){
+
+    if (nDim == 2){ // Case 2D model
+
+        for(int iNod=0; iNod<nTotNodes; iNod++){
+
+            std::get<std::vector<ColVecd2>>(nodFlux).at(iNod).setZero();
+            nodCount.at(iNod) = 0;
+
+        }
+
+    } else if (nDim == 3){ // Case 3D
+
+        for(int iNod=0; iNod<nTotNodes; iNod++){
+
+            std::get<std::vector<ColVecd3>>(nodFlux).at(iNod).setZero();
+            nodCount.at(iNod) = 0;
+            
+        }
+    }
+}
+
+void TransportModel::InitializePETSc(vector<BaseElemTrans*> elements){
+
+    // TODO: For debug!
+    // for(auto* elem : elements)
+    //     for (int dispDof : elem->get_elemDispDof(0))
+    //         cout << dispDof << "\n";
+
+    // Initialize the vectors
+    VecCreate(PETSC_COMM_WORLD, &F);
+    VecSetSizes(F, PETSC_DECIDE, nTotDofs);
+
+    VecSetType(F, VECSEQ);
+    VecDuplicate(F, &x);      
+
+    VecSet(F, 0.0); // Set all values to zero.
+    VecAssemblyBegin(F); VecAssemblyEnd(F);
+
+    VecSet(x, 0.0); // Set all values to zero.
+    VecAssemblyBegin(x); VecAssemblyEnd(x);
+
+    // Initialize the coefficient matrices.
+    MatCreate(PETSC_COMM_WORLD, &K);
+    MatSetSizes(K, PETSC_DECIDE, PETSC_DECIDE, nTotDofs, nTotDofs);
+    
+    MatSetType(K, MATSEQAIJ); 
+
+    MatDuplicate(K, MAT_DO_NOT_COPY_VALUES, &M);
+
+    // Preallocate the coefficient matrix.
+    vector<vector<int>> gDofs(nTotDofs); // vector to store dofs per row.
+    int itotv, jtotv; // for global row and colum dof.
+    PetscInt *nnz; // Array for the number of zeros per row
+    PetscMalloc1(nTotDofs, &nnz); // Allocates the size of nnz
+    
+    // Find the number of non zeros (columns) per row for preallocation.
+    for (auto* elem : elements){  // Loop through element sets
+
+        nElConDofs = elem->get_nElConDofs();
+        nElements = elem->get_nElements();
+        const vector<vector<int>>& elemConDof_ptr = elem->get_elemConDof();
+    
+        for (int iElem=0; iElem<nElements; iElem++){ // Loop through all elements per element set
+
+            for (int idof=0; idof<nElConDofs; idof++){ // Row
+
+                itotv = elemConDof_ptr.at(iElem).at(idof); // global row number
+                
+                for (int jdof=0; jdof<nElConDofs; jdof++){// Column.
+
+                    jtotv = elemConDof_ptr.at(iElem).at(jdof); // global column number
+
+                    // If jtotv is not in gDofs.at(itotv)
+                    if (find(gDofs.at(itotv).begin(), gDofs.at(itotv).end(), jtotv) == gDofs.at(itotv).end()){
+
+                        gDofs.at(itotv).push_back(jtotv);
+                    }
+                }
+            }
+        }
+    }   
+
+    // Add the number of non zero columns to nnz
+    for (int iDof=0; iDof<nTotDofs; iDof++){
+        nnz[iDof] = gDofs.at(iDof).size();
+    }
+
+    // Preallocate the stiffness matrix.
+    MatSeqAIJSetPreallocation(K, PETSC_DEFAULT, nnz); 
+    MatSeqAIJSetPreallocation(M, PETSC_DEFAULT, nnz); 
+    PetscFree(nnz);
+
+}
+
+void TransportModel::CalcElemStiffMatx(vector<BaseElemTrans*> elements, vector<BaseTransport*> mats){
+
+    for (int iSet=0; iSet<nElementSets; iSet++){
+        // Downcasting to derived class `HeatMassTransport`
+        elements[iSet]->CalcElemStiffMatx(mats[iSet]->getKMatx(), dynamic_cast<HeatMassTransport*>(mats[iSet])->getCapacity());
+    }
+}
+
+void TransportModel::Assemble(vector<BaseElemTrans*> elements){
+
+    for (auto* elem : elements){  // Loop through element sets
+
+        const vector<vector<int>>& elemConDof_ptr = elem->get_elemConDof();
+        nElConDofs = elem->get_nElConDofs();
+        nElements = elem->get_nElements();
+
+        //  Assemble the coefficient matrix.        
+        PetscInt* i1; PetscInt* j1; // Indices for row and columns to insert.
+        PetscMalloc1(nElConDofs, &i1);
+        PetscMalloc1(nElConDofs, &j1);
+
+        const T_ElStiffMatx& T_elStiffMatx_ref = elem->getElStiffMatx();
+        const T_ElStiffMatx& T_elCapMatx_ref = elem->getElCapMatx();
+
+        if (std::holds_alternative<vector<Matd4x4>*>(T_elStiffMatx_ref)){  // Quad4 elements.
+ 
+            const vector<Matd4x4>& elStiffMatx_ref = *std::get<vector<Matd4x4>*>(T_elStiffMatx_ref);
+            const vector<Matd4x4>& elCapMatx_ref = *std::get<vector<Matd4x4>*>(T_elCapMatx_ref);
+
+            for (int iElem =0; iElem<nElements; iElem++){ // Loop through elements
+
+                // Get the disp dofs associated with the element
+                for(int iElDof=0; iElDof<nElConDofs; iElDof++){
+
+                    i1[iElDof] = elemConDof_ptr.at(iElem).at(iElDof);
+                    j1[iElDof] = elemConDof_ptr.at(iElem).at(iElDof);
+
+                }
+
+                MatSetValues(K, nElConDofs, i1, nElConDofs, j1, elStiffMatx_ref.at(iElem).data(), ADD_VALUES);
+                MatSetValues(M, nElConDofs, i1, nElConDofs, j1, elCapMatx_ref.at(iElem).data(), ADD_VALUES);
+            }
+
+        // } else if (std::holds_alternative<vector<Matd6x6>*>(T_elStiffMatx_ref)){  // Tri3 elements.
+ 
+        //     const vector<Matd6x6>& elStiffMatx_ref = *std::get<vector<Matd6x6>*>(T_elStiffMatx_ref);
+
+        //     for (int iElem =0; iElem<nElements; iElem++){ // Loop through elements
+
+        //         // Get the disp dofs associated with the element
+        //         for(int iElDof=0; iElDof<nElConDofs; iElDof++){
+
+        //             i1[iElDof] = elemConDof_ptr.at(iElem).at(iElDof);
+        //             j1[iElDof] = elemConDof_ptr.at(iElem).at(iElDof);
+        //         }
+
+        //         MatSetValues(K, nElConDofs, i1, nElConDofs, j1, elStiffMatx_ref.at(iElem).data(), ADD_VALUES);
+        //     }
+
+        // } else if (std::holds_alternative<vector<Matd24x24>*>(T_elStiffMatx_ref)){  // Hex8 elements.
+ 
+        //     const vector<Matd24x24>& elStiffMatx_ref = *std::get<vector<Matd24x24>*>(T_elStiffMatx_ref);
+
+        //     for (int iElem =0; iElem<nElements; iElem++){ // Loop through elements
+
+        //         // Get the disp dofs associated with the element
+        //         for(int iElDof=0; iElDof<nElConDofs; iElDof++){
+
+        //             i1[iElDof] = elemConDof_ptr.at(iElem).at(iElDof);
+        //             j1[iElDof] = elemConDof_ptr.at(iElem).at(iElDof);
+        //         }
+
+        //         MatSetValues(K, nElConDofs, i1, nElConDofs, j1, elStiffMatx_ref.at(iElem).data(), ADD_VALUES);
+        //     }
+        }
+
+        // Free memory
+        PetscFree(i1);
+        PetscFree(j1);
+    }
+
+    MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY);  MatAssemblyEnd(K, MAT_FINAL_ASSEMBLY);
+    MatSetOption(K, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE);
+
+    MatAssemblyBegin(M, MAT_FINAL_ASSEMBLY);  MatAssemblyEnd(M, MAT_FINAL_ASSEMBLY);
+    MatSetOption(M, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE);
+}
+
+void TransportModel::InitializeDirichBC(H5IO& H5File_in){
+
+    // Read Dirichlet BCs
+    string dsetName;
+    dsetName = "SimulationParameters/nPresDofs";
+    nPresDofs = H5File_in.ReadScalar(dsetName);
+    PetscMalloc1(nPresDofs, &presDofs);
+    PetscMalloc1(nPresDofs, &presVals); 
+
+    vector<double> dummy(3);
+    for (int iPresDof=0; iPresDof<nPresDofs; iPresDof++){
+        // Read values
+        dsetName = "PrescribedDOFs/Prescribed_"+to_string(iPresDof);
+        H5File_in.ReadFieldDoub1D(dsetName, dummy);
+        // Assign values
+        presDofs[iPresDof] = dummy.at(0); // nDim*iNode+dof
+        presVals[iPresDof] = dummy.at(1);
+        // // TODO: For debug!
+        // cout << presDofs[iPresDof] << " --> " << presVals[iPresDof] << "\n";
+    }
+
+    // For the coefficient matrix
+    MatZeroRows(K, nPresDofs, presDofs, 1.0, NULL, NULL);
+    MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY);  MatAssemblyEnd(K, MAT_FINAL_ASSEMBLY);
+}
+
+void TransportModel::setDirichBC(){
+    
+    MatMult(M, x, F);
+
+    VecSetValues(F, nPresDofs, presDofs, presVals, INSERT_VALUES); 
+    VecAssemblyBegin(F); VecAssemblyEnd(F);
+
+    // VecView(F, PETSC_VIEWER_STDOUT_WORLD);
+}
+
+int TransportModel::get_nSteps() const{
+    
+    return nSteps;
+}
+
+Vec& TransportModel::getF(){
+
+    return F;
+}
+
+Vec& TransportModel::getX(){
+
+    return x;
+}
+
+Mat& TransportModel::getK(){
+
+    return K;
+}
+
+// void TransportModel::CalcStres(vector<BaseElemTrans*> elements, vector<BaseTransport*> mats){
+
+//     VecGetArrayRead(x, &globalBuffer);
+
+//     for (int iSet=0; iSet<nElementSets; iSet++){
+        
+//         elements[iSet]->CalcStres(mats[iSet]->getDMatx(), globalBuffer, Fint, nodStres, nodStran, nodCount);
+//     }
+
+//     // Number averaging the nodal values
+//     if (nDim==2){
+
+//         for(int iNod=0; iNod<nTotNodes; iNod++){
+            
+//             std::get<std::vector<ColVecd3>>(nodStran).at(iNod) = std::get<std::vector<ColVecd3>>(nodStran).at(iNod)/nodCount.at(iNod);
+//             std::get<std::vector<ColVecd3>>(nodStres).at(iNod) = std::get<std::vector<ColVecd3>>(nodStres).at(iNod)/nodCount.at(iNod);
+//         }
+
+//     } else if (nDim==3){
+
+//         for(int iNod=0; iNod<nTotNodes; iNod++){
+            
+//             std::get<std::vector<ColVecd6>>(nodStran).at(iNod) = std::get<std::vector<ColVecd6>>(nodStran).at(iNod)/nodCount.at(iNod);
+//             std::get<std::vector<ColVecd6>>(nodStres).at(iNod) = std::get<std::vector<ColVecd6>>(nodStres).at(iNod)/nodCount.at(iNod);
+//         }
+
+//     }
+
+//     // TODO: For debug!
+//     // cout << std::get<std::vector<ColVecd3>>(nodStran).at(0) << "\n";
+
+//     VecRestoreArrayRead(x, &globalBuffer);
+// }
+
+void TransportModel::WriteOut(vector<BaseElemTrans*> elements, H5IO &H5File_out, const string iStep){
+
+    // Displacements
+    VecGetArrayRead(x, &globalBuffer);
+    H5File_out.WriteArray_1D("Con/Step_"+iStep, nTotDofs, globalBuffer);
+    // VecRestoreArrayRead(x, &globalBuffer);
+    // H5File_out.WriteArray_1D("Force/Step_"+iStep, nTotDofs, Fint);
+
+    // // Stresses and strains
+    // if (nDim==2){
+    //     H5File_out.WriteStres("Strain/Step_"+iStep, nTotNodes, 2, nodStran);
+    //     // H5File_out.WriteStres("Stress/Step_"+iStep, nTotNodes, 3, nodStres);
+    // } else if (nDim==3) {
+    //     H5File_out.WriteStres("Strain/Step_"+iStep, nTotNodes, 3, nodStran);
+    //     // H5File_out.WriteStres("Stress/Step_"+iStep, nTotNodes, 6, nodStres);
+    // }
+
+    // // set zeros
+    // setZero_nodStres();
+}
---
+++ src/include/Models/TransportModel.h
@@ -0,0 +1,153 @@
+/**
+ * @file TransportModel.h
+ * @brief A class based on heat and mass transport models to interface with PETSc based global
+ * capacitance matrix `C`, conductivity matrix `K`, solution vector `x` and RHS `F`.
+ * 
+ * @details Main functions:
+ * - Calls `BaseElemTrans::CalcElemStiffMatx` to build the local diffusivity and capacitance matrix.
+ * - Assembles the global capacitance and conductivity matrix.
+ * - Initializes and applies boundary conditions.
+ * - Manages the output by writing to H5file_out.
+ * 
+ * @author Abdelrahman Hussein (a.h.a.hussein@outlook.com)
+ * @date 2024-06-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef TRANSPORTMODEL_H
+#define TRANSPORTMODEL_H
+
+#include <petscsys.h>
+#include <petscvec.h>
+#include <petscmat.h>
+#include "H5IO.h"
+#include "FiniteElements/Transport/BaseElemTrans.h"
+#include "Materials/Transport/BaseTransport.h"
+
+class TransportModel{
+
+public:
+
+TransportModel(vector<BaseElemTrans*> elements, H5IO& H5File_in);
+~TransportModel();
+
+/**
+ * @brief Set nodal quantities `nodStres`, `nodStran` and `nodCount` to zeros.
+ * 
+ */
+void setZero_nodFlux();
+
+/**
+ * @brief Initializes and preallocates the RHS `F`, solution `x` and 
+ *        conductivity matrix `K` and capacitance matrix `C`.
+ * 
+ * @param elements 
+ */
+void InitializePETSc(vector<BaseElemTrans*> elements);
+
+/**
+ * @brief Calculates the element stiffness matrix. 
+ * 
+ * @param elements 
+ * @param mats 
+ */
+void CalcElemStiffMatx(vector<BaseElemTrans*> elements, vector<BaseTransport*> mats);
+
+/**
+ * @brief Assemble the global stiffness matrix.
+ * 
+ */
+void Assemble(vector<BaseElemTrans*> elements);
+
+/**
+ * @brief Reads and initializes Dirichlet BCs.
+ * 
+ * @param H5File_in 
+ */
+void InitializeDirichBC(H5IO& H5File_in);
+
+/**
+ * @brief Set Dirichlet boundary conditions in the RHS `b` and global stiffness matrix `A`.
+ * 
+ */
+void setDirichBC();
+
+/**
+ * @brief Get the number of steps to apply load.
+ * 
+ * @return int 
+ */
+int get_nSteps() const;
+
+/**
+ * @brief Pass reference of RHS (to solver).
+ * 
+ * @return Vec& 
+ */
+Vec& getF();
+
+/**
+ * @brief Pass a reference of the solution vector (to solver).
+ * 
+ * @return Vec& 
+ */
+Vec& getX();
+
+/**
+ * @brief Pass a reference fo the stiffness matrix (to solver).
+ * 
+ * @return Mat& 
+ */
+Mat& getK();
+
+// /**
+//  * @brief Calculates the Fint, strains and stresses. Also Calculates the stress nodal values 
+//  *        if `nodStresFlag=true`.
+//  * 
+//  * @param elements 
+//  * @param nodStresFlag 
+//  */
+// void CalcStres(vector<BaseElemTrans*> elements, vector<BaseTransport*> mats);
+
+/**
+ * @brief Write nodal values.
+ * 
+ * @param elements 
+ * @param H5File_out 
+ */
+void WriteOut(vector<BaseElemTrans*> elements, H5IO &H5File_out, const string iStep);
+
+private:
+
+int nElementSets;   /// @brief Number of element sets
+int nTotNodes;      /// @brief Total number of nodes.
+int nTotDofs;       /// @brief Total number of DOFs.
+int nTotElements;   /// @brief Total number of elements.
+int nDim;           /// @brief Spatial dimensions of the model.
+int nElConDofs;     /// @brief Number of element concentration (temp) dofs.
+int nElements;      /// @brief Number of elements per element set.
+int nSteps;         /// @brief Number of steps to apply the load.
+double dt;          /// @brief Time increment.
+
+T_nodFlux nodFlux;        /// @brief Nodal flux.
+vector<int> nodCount;     /// @brief Counter for integration points surrounding nodes.
+
+// PETSc ------------------------
+
+const double* globalBuffer;  /// @brief buffer array for PETSc data
+double* Fint = NULL;         /// @brief For calculating the internal force vector.
+
+// Boundary conditions
+PetscInt nPresDofs;            /// @brief number of prescribed dofs.
+PetscInt* presDofs = NULL;     /// @brief Array to hold the prescribed dofs.
+PetscScalar* presVals = NULL;  /// @brief Array to hold the prescribed values.
+
+Vec F;   /// @brief RHS vector.
+Vec x;   /// @brief solution vector.
+Mat K;   /// @brief The global conductivity matrix.
+Mat M;   /// @brief The global capacitance matrix.
+
+};
+#endif
