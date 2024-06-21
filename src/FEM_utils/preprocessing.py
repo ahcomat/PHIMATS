4b4f5ac2932c3b8dfcf176372e3b2e80d7d923b3
AbduKT <abdelra7man87@gmail.com>
2024-06-21 15:37:42 +0200

    Extend for `PhaseTrapping` model

--- src/FEM_utils/preprocessing.py
+++ src/FEM_utils/preprocessing.py
@@ -22,9 +22,25 @@ class PreProcessing:
         self.nPresDofs = len(self.presBCs)
         self.nSteps = inputData["nSteps"]
         
+        # Allowed simulation types
+        allowedSimulTypes = ["Mechanical", "Transport", "PhaseTrapping"]
+                
+        if not self.SimulType in allowedSimulTypes:
+            ErrString = "ERROR! Unknown simulation type < " + self.SimulType + " >\n"
+            ErrString += "Allowed elements are: \n"
+            for simulType in allowedSimulTypes:
+                ErrString += simulType + "\n"
+            raise ValueError(ErrString)
+        
         if self.SimulType=="Transport":
             self.exitNods = inputData["exitNods"]
             self.dt = inputData["dt"]
+        elif self.SimulType=="PhaseTrapping":
+            self.exitNods = inputData["exitNods"]
+            self.dt = inputData["dt"]
+            self.R = inputData["R"]
+            self.T = inputData["T"]
+            self.mesh2 = inputData["mesh2"]
 
         #----------------------------------------------------------------------
         # Check for allowed elements and assign element data (number of nodes,
@@ -89,6 +105,8 @@ class PreProcessing:
             self.nTotDofs = self.nTotNodes*self.nDim
         elif self.SimulType == "Transport":
             self.nTotDofs = self.nTotNodes
+        elif self.SimulType == "PhaseTrapping":
+            self.nTotDofs = self.nTotNodes
         
         #----------------------------------------------------------------------
         # Read number of elements  
@@ -100,16 +118,6 @@ class PreProcessing:
         #----------------------------------------------------------------------
         # Read material data
         #----------------------------------------------------------------------
-        
-        # Allowed simulation types
-        allowedSimulTypes = ["Mechanical", "Transport"]
-                
-        if not self.SimulType in allowedSimulTypes:
-            ErrString = "ERROR! Unknown simulation type < " + self.SimulType + " >\n"
-            ErrString += "Allowed elements are: \n"
-            for simulType in allowedSimulTypes:
-                ErrString += simulType + "\n"
-            raise ValueError(ErrString)
         
         # Read materials dict
         self.Materials = inputData["Materials"]
@@ -130,112 +138,140 @@ class PreProcessing:
         # Write simulation parameters to hdf5  
         #----------------------------------------------------------------------
         
-        try :
+        # try :
             
-            # self.fh5.attrs["Simulation"] =  self.Simul
-                        
-            #----------------------------------------------------------------------
-            # Nodes/Elements data 
-            #----------------------------------------------------------------------
-            
-            self.grp_Sim_Params = self.fh5.create_group('SimulationParameters')
-
-            self.grp_Sim_Params.create_dataset("nDim", data=self.nDim, dtype = np.int64)
-            self.grp_Sim_Params.create_dataset("nTotNodes", data=self.nTotNodes, dtype = np.int64)
-            self.grp_Sim_Params.create_dataset("nTotDofs", data=self.nTotDofs, dtype = np.int64)
-            self.grp_Sim_Params.create_dataset("nTotElements", data=self.nTotElements, dtype = np.int64)
-            self.grp_Sim_Params.create_dataset("nPresDofs", data=self.nPresDofs, dtype = np.int64)
-            self.grp_Sim_Params.create_dataset("nElementSets", data=self.nElementSets, dtype = np.int64)
-            self.grp_Sim_Params.create_dataset("nSteps", data=self.nSteps, dtype = np.int64)
-            
-            if self.SimulType == "Transport":
-                self.grp_Sim_Params.create_dataset("dt", data=self.dt)
-            
-            #----------------------------------------------------------------------
-            # Material data
-            #----------------------------------------------------------------------
-            self.grp_Materials = self.fh5.create_group('Materials')
-            
-            if self.SimulType == "Mechanical":
-            
-                counter = 0
-                for mat in self.Materials:
-                    counter+=1
-                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/Emod", data=self.Materials[mat]["Emod"])
-                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/nu", data=self.Materials[mat]["nu"])
+        # self.fh5.attrs["Simulation"] =  self.Simul
                     
-            elif self.SimulType == "Transport":
-                
-                counter = 0
-                for mat in self.Materials:
-                    counter+=1
-                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/xKappa", data=self.Materials[mat]["xKappa"])
-                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/yKappa", data=self.Materials[mat]["yKappa"])
-                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/c", data=self.Materials[mat]["c"])
-                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/rho", data=self.Materials[mat]["rho"])
-
-                    if self.nDim == 3:
-                        self.grp_Materials.create_dataset("Material_"+str(counter)+"/yKappa", data=self.Materials[mat]["zKappa"])
-                    
-            #----------------------------------------------------------------------
-            # Write node coordinates 
-            #----------------------------------------------------------------------
-                
-            self.fh5.create_dataset("NodeCoordinates", data=self.nodeCoord, dtype = np.float64)
-                        
-            #----------------------------------------------------------------------
-            # Write element node connectivity
-            #----------------------------------------------------------------------
-            
-            # All elements
-            self.fh5.create_dataset("NodeConnectivity", data=self.nodeConnectivity, dtype = np.int64)
-
-            # Data per element set
-            # element connectivity
-            counter = 0
-            for elSet in self.mesh.cell_sets_dict.values():
-                counter+=1
-                self.grp_elemSet = self.fh5.create_group('Elements/ElementSet_'+str(counter))
-                elemIDs = list(elSet.values())[0]
-                nElems = elemIDs.shape[0]
-                print(nElems)
-                self.grp_elemSet.create_dataset("nElements", data=nElems, dtype = np.int64)
-                self.grp_elemSet.create_dataset("ElementSetIDs", data=elemIDs, dtype = np.int64)  
-            # Nodes
-            counter = 0
-            for pSet in self.mesh.point_sets:
-                counter+=1
-                elNodes = self.mesh.point_sets[pSet]
-                nElTotNodes = len(elNodes)
-                print(len(self.mesh.point_sets[pSet]))
-                self.fh5.create_dataset('Elements/ElementSet_'+str(counter)+'/nNodes', data=nElTotNodes, dtype = np.int64)
-                self.fh5.create_dataset('Elements/ElementSet_'+str(counter)+'/NodeSetIDs', data=elNodes, dtype = np.int64)  
-            
-            #----------------------------------------------------------------------
-            # Write prescribed dofs
-            #----------------------------------------------------------------------
-            
-            self.grp_prescribedDOFs = self.fh5.create_group('PrescribedDOFs')
-            
-            for iPreDof in range(self.nPresDofs):
-                self.grp_prescribedDOFs.create_dataset("Prescribed_"+str(iPreDof), data=self.presBCs[iPreDof]) 
-                
-            if self.SimulType == "Transport":
-                self.grp_prescribedDOFs = self.fh5.create_group('ExitNodes')
-            
-                for iENode in range(len(self.exitNods)):
-                    self.grp_prescribedDOFs.create_dataset("Exit_"+str(iENode), data=iENode)
-
-            #----------------------------------------------------------------------
-            # Close hdf5 file
-            #----------------------------------------------------------------------
-            
-            self.CloseFileHDF5()
+        #----------------------------------------------------------------------
+        # Nodes/Elements data 
+        #----------------------------------------------------------------------
         
-        except:
-            # Close hdf5 file
-            self.CloseFileHDF5()
-            print("Some error occurred with hdf5 file handle. The file is correctly closed.")
+        self.grp_Sim_Params = self.fh5.create_group('SimulationParameters')
+
+        self.grp_Sim_Params.create_dataset("nDim", data=self.nDim, dtype = np.int64)
+        self.grp_Sim_Params.create_dataset("nTotNodes", data=self.nTotNodes, dtype = np.int64)
+        self.grp_Sim_Params.create_dataset("nTotDofs", data=self.nTotDofs, dtype = np.int64)
+        self.grp_Sim_Params.create_dataset("nTotElements", data=self.nTotElements, dtype = np.int64)
+        self.grp_Sim_Params.create_dataset("nPresDofs", data=self.nPresDofs, dtype = np.int64)
+        self.grp_Sim_Params.create_dataset("nElementSets", data=self.nElementSets, dtype = np.int64)
+        self.grp_Sim_Params.create_dataset("nSteps", data=self.nSteps, dtype = np.int64)
+        
+        if self.SimulType == "Transport":
+            self.grp_Sim_Params.create_dataset("dt", data=self.dt)
+        
+        # Case Trapping 
+        if self.SimulType == "PhaseTrapping":
+            self.grp_Sim_Params.create_dataset("dt", data=self.dt)
+            self.grp_Sim_Params.create_dataset("R", data=self.R)
+            self.grp_Sim_Params.create_dataset("T", data=self.T)
+            
+            phi = np.zeros(len(self.mesh2.points))
+            for iNode in range(len(self.mesh2.points)):
+                if iNode in self.mesh2.point_sets.get('trap'):
+                    phi[iNode] = 1
+                    
+            self.fh5.create_dataset('Phi', data=phi, dtype = np.float64)  
+        
+        #----------------------------------------------------------------------
+        # Material data
+        #----------------------------------------------------------------------
+        self.grp_Materials = self.fh5.create_group('Materials')
+        
+        if self.SimulType == "Mechanical":
+        
+            counter = 0
+            for mat in self.Materials:
+                counter+=1
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/Emod", data=self.Materials[mat]["Emod"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/nu", data=self.Materials[mat]["nu"])
+                
+        elif self.SimulType == "Transport":
+            
+            counter = 0
+            for mat in self.Materials:
+                counter+=1
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/Dx", data=self.Materials[mat]["Dx"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/Dy", data=self.Materials[mat]["Dy"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/s", data=self.Materials[mat]["s"])
+
+                if self.nDim == 3:
+                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/Dy", data=self.Materials[mat]["Dz"])
+                    
+        elif self.SimulType == "PhaseTrapping":
+            for mat in self.Materials:
+                counter = 1
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/D0x1", data=self.Materials[mat]["D0x1"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/D0y1", data=self.Materials[mat]["D0y1"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/DQx1", data=self.Materials[mat]["DQx1"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/DQy1", data=self.Materials[mat]["DQy1"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/D0x2", data=self.Materials[mat]["D0x2"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/D0y2", data=self.Materials[mat]["D0y2"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/DQx2", data=self.Materials[mat]["DQx2"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/DQy2", data=self.Materials[mat]["DQy2"])
+                self.grp_Materials.create_dataset("Material_"+str(counter)+"/EMM", data=self.Materials[mat]["EMM"])
+
+                if self.nDim == 3:
+                    self.grp_Materials.create_dataset("Material_"+str(counter)+"/Dz", data=self.Materials[mat]["Dz"])
+                
+        #----------------------------------------------------------------------
+        # Write node coordinates 
+        #----------------------------------------------------------------------
+            
+        self.fh5.create_dataset("NodeCoordinates", data=self.nodeCoord, dtype = np.float64)
+                    
+        #----------------------------------------------------------------------
+        # Write element node connectivity
+        #----------------------------------------------------------------------
+        
+        # All elements
+        self.fh5.create_dataset("NodeConnectivity", data=self.nodeConnectivity, dtype = np.int64)
+
+        # Data per element set
+        # element connectivity
+        counter = 0
+        for elSet in self.mesh.cell_sets_dict.values():
+            counter+=1
+            self.grp_elemSet = self.fh5.create_group('Elements/ElementSet_'+str(counter))
+            elemIDs = list(elSet.values())[0]
+            nElems = elemIDs.shape[0]
+            print(nElems)
+            self.grp_elemSet.create_dataset("nElements", data=nElems, dtype = np.int64)
+            self.grp_elemSet.create_dataset("ElementSetIDs", data=elemIDs, dtype = np.int64)  
+        # Nodes
+        counter = 0
+        for pSet in self.mesh.point_sets:
+            counter+=1
+            elNodes = self.mesh.point_sets[pSet]
+            nElTotNodes = len(elNodes)
+            print(len(self.mesh.point_sets[pSet]))
+            self.fh5.create_dataset('Elements/ElementSet_'+str(counter)+'/nNodes', data=nElTotNodes, dtype = np.int64)
+            self.fh5.create_dataset('Elements/ElementSet_'+str(counter)+'/NodeSetIDs', data=elNodes, dtype = np.int64)  
+        
+        #----------------------------------------------------------------------
+        # Write prescribed dofs
+        #----------------------------------------------------------------------
+        
+        self.grp_prescribedDOFs = self.fh5.create_group('PrescribedDOFs')
+        
+        for iPreDof in range(self.nPresDofs):
+            self.grp_prescribedDOFs.create_dataset("Prescribed_"+str(iPreDof), data=self.presBCs[iPreDof]) 
+            
+        if self.SimulType == "Transport":
+            self.grp_prescribedDOFs = self.fh5.create_group('ExitNodes')
+        
+            for iENode in range(len(self.exitNods)):
+                self.grp_prescribedDOFs.create_dataset("Exit_"+str(iENode), data=iENode)
+
+        #----------------------------------------------------------------------
+        # Close hdf5 file
+        #----------------------------------------------------------------------
+        
+        self.CloseFileHDF5()
+    
+        # except:
+        #     # Close hdf5 file
+        #     self.CloseFileHDF5()
+        #     print("Some error occurred with hdf5 file handle. The file is correctly closed.")
             
         pass
 
