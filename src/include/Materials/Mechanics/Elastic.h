c8e1191d6746047c24f6bebd3c770634db7dd6b9
AbduKT <abdelra7man87@gmail.com>
2024-12-15 13:40:27 +0200

    Update documentation

--- src/include/Materials/Mechanics/Elastic.h
+++ src/include/Materials/Mechanics/Elastic.h
@@ -66,7 +66,7 @@ T_DMatx getDMatx() const override;
 
 private:
 
-T_DMatx DMatx;      /// @brief The elastic stiffness matrix in Voigt notation.
+T_DMatx DMatx;      /// @brief The stiffness matrix in Voigt notation.
 
 };
 #endif
