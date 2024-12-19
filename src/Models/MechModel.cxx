896359e798435c9f05a37c623360d23a73ec837c
AbduKT <abdelra7man87@gmail.com>
2024-12-19 07:58:41 +0200

    Update material includes

--- src/Models/MechModel.cxx
+++ src/Models/MechModel.cxx
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<variant>
 
-#include"Models/MechModel.h"
+#include "Models/MechModel.h"
+#include "Materials/Mechanics/Elastic.h"
+#include "Materials/Mechanics/IsoHard.h"
 
 using namespace std;
 
