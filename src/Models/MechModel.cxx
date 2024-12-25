cf25e02e13b9b4472c1302cbdf21627a91730f03
ahcomat <abdelra7man87@gmail.com>
2024-12-25 11:02:31 +0200

    Sets final sparsity of matA

--- src/Models/MechModel.cxx
+++ src/Models/MechModel.cxx
@@ -367,8 +367,8 @@ PetscErrorCode MechModel::Assemble(vector<BaseElemMech*> elements){
     // For Dirichlet boundary conditions (Requires `MAT_FINAL_ASSEMBLY`)
     MatZeroRows(matA, nPresDofs, presDofs, 1.0, NULL, NULL);
 
-    // // Sets the final sparsity structure  
-    // MatSetOption(matA, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE);
+    // Sets the final sparsity structure  
+    MatSetOption(matA, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE);
 
     return 0;
 }
@@ -483,8 +483,6 @@ PetscErrorCode MechModel::CalcResidual(Vec deltaU, vector<BaseElemMech*> element
         cerr << "Terminating!" << endl;
         exit(EXIT_FAILURE);
     }
-
-    Assemble(elements);
 
     return 0;
 
