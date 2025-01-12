491540f9bf3024844e0a33bf71fc99be13dd0e20
ahcomat <abdelra7man87@gmail.com>
2025-01-12 16:41:23 +0200

    Add `Version.h` for versioning

---
+++ src/include/Version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
+
+#define VERSION_STRING "v0.2.0"
+#define VERSION_DATE "2025-01-10"  // Release date
+
+#endif // VERSION_H
