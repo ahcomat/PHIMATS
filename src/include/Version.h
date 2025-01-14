371aa5f1b1bd44725838f5687b96bdb99fae2ae5
ahcomat <abdelra7man87@gmail.com>
2025-01-14 14:33:27 +0200

    v0.3.0

--- src/include/Version.h
+++ src/include/Version.h
@@ -5,9 +5,9 @@
 #define VERSION_MINOR 2
 #define VERSION_PATCH 0
 
-#define VERSION_STRING "v0.2.0"
-#define VERSION_DATE "2025-01-10"  // Release date
-#define PROJECT_WEBSITE "https://yourwebsite.com"
+#define VERSION_STRING "v0.3.0"
+#define VERSION_DATE "2025-01-14"  // Release date
+#define PROJECT_WEBSITE "https://comp-mat.com/phimats"
 #define PROJECT_CITATION "Your Name, PhiMATS, 2025, DOI:10.xxxx/phiMATS"
 
 #endif // VERSION_H
