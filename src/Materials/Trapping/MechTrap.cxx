baf7c49e805b8688647be01b1c97416f3d5ec604
ahcomat <abdelra7man87@gmail.com>
2025-01-27 12:12:27 +0200

    Extension for 3D

--- src/Materials/Trapping/MechTrap.cxx
+++ src/Materials/Trapping/MechTrap.cxx
@@ -9,22 +9,31 @@ MechTrap::MechTrap(string dimensions, H5IO& H5File, int iSet, Logger& logger)
 
     string dsetName;
 
-    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/D0x1";
+    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/D0x";
     D0x = H5File.ReadScalar(dsetName);
 
-    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/D0y1";
+    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/D0y";
     D0y = H5File.ReadScalar(dsetName);
 
-    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/DQx1";
+    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/DQx";
     DQx = H5File.ReadScalar(dsetName);
 
-    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/DQy1";
+    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/DQy";
     DQy = H5File.ReadScalar(dsetName);
 
     dsetName = "Materials/Material_"+ std::to_string(iSet)+"/Vh";
     Vh = H5File.ReadScalar(dsetName);
 
+    dsetName = "Materials/Material_"+ std::to_string(iSet)+"/m";
+    m = H5File.ReadScalar(dsetName);
+
     if (dims=="3D"){
+
+        dsetName = "Materials/Material_"+ std::to_string(iSet)+"/D0z";
+        D0z = H5File.ReadScalar(dsetName);
+
+        dsetName = "Materials/Material_"+ std::to_string(iSet)+"/DQz";
+        DQz = H5File.ReadScalar(dsetName);
 
     }
 }
@@ -34,21 +43,32 @@ T_DMatx MechTrap::CalcKMatx(const double T){
     double DLx = D0x*exp(-DQx/(T*R));
     double DLy = D0y*exp(-DQy/(T*R));
 
-    // Variant for storing the diffusivity (conductivity) matrix
+    // Variant for storing the diffusivity matrix
     T_DMatx KMatx;
 
     if (dims=="2D"){
 
-    Matd2x2 mat2 = Matd2x2::Zero();
+        Matd2x2 mat2 = Matd2x2::Zero();
     
-    mat2.setZero();
-    mat2(0, 0) = DLx;
-    mat2(1, 1) = DLy;
+        mat2.setZero();
+        mat2(0, 0) = DLx;
+        mat2(1, 1) = DLy;
 
-    KMatx = mat2;
+        KMatx = mat2;
 
     } else if (dims=="3D"){
+
+        double DLz = D0z*exp(-DQz/(T*R));
+
+        Matd3x3 mat3 = Matd3x3::Zero();
     
+        mat3.setZero();
+        mat3(0, 0) = DLx;
+        mat3(1, 1) = DLy;
+        mat3(2, 2) = DLz;
+
+        KMatx = mat3;
+
     }
 
     return KMatx;
@@ -64,15 +84,26 @@ T_DMatx MechTrap::CalcTMatx(const double T){
 
     if (dims=="2D"){
 
-    Matd2x2 mat2 = Matd2x2::Zero();
-    
-    mat2.setZero();
-    mat2(0, 0) = (DLx*Vh)/(R*T);
-    mat2(1, 1) = (DLy*Vh)/(R*T);
+        Matd2x2 mat2 = Matd2x2::Zero();
+        
+        mat2.setZero();
+        mat2(0, 0) = (DLx*Vh)/(R*T);
+        mat2(1, 1) = (DLy*Vh)/(R*T);
 
-    TMatx = mat2;
+        TMatx = mat2;
 
     } else if (dims=="3D"){
+
+        double DLz = D0z*exp(-DQz/(T*R));
+
+        Matd3x3 mat3 = Matd3x3::Zero();
+        
+        mat3.setZero();
+        mat3(0, 0) = (DLx*Vh)/(R*T);
+        mat3(1, 1) = (DLy*Vh)/(R*T);
+        mat3(2, 2) = (DLz*Vh)/(R*T);
+
+        TMatx = mat3;
     
     }
 
