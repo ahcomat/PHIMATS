fa560f7dc0cf0aa6e62652fcbd6546c8406db697
ahcomat <abdelra7man87@gmail.com>
2025-03-07 14:56:31 +0200

    move `ReadNodalStress` to Base

--- src/include/FiniteElements/Trapping/Tri3TH.h
+++ src/include/FiniteElements/Trapping/Tri3TH.h
@@ -104,8 +104,6 @@ void getInPtCoords(T_nodStres& glIntPtCoords) override;
 //  */
 // void CalcGrad(T_nodStres& nodGrad, vector<double>& nodCount, double* nodLapPhi) override;
 
-void ReadNodalStress(H5IO &H5File_stress, int iStep) override;
-
 /**
  * @brief Calculates the element stiffness and capacitance matrix for all elements.
  * 
