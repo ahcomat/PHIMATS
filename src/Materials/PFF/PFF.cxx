4a70925e6bf34c6e5782a39c6bb360c7962aa170
ahcomat <abdelra7man87@gmail.com>
2026-01-30 10:43:05 +0200

    style: update error message

--- src/Materials/PFF/PFF.cxx
+++ src/Materials/PFF/PFF.cxx
@@ -11,10 +11,13 @@ PFF::PFF(string dimensions, H5IO& H5File, int iSet, Logger& logger)
         wc = H5File.ReadScalar("Materials/Material_" + to_string(iSet)+"/PFF/wc");
         const_ell = H5File.ReadScalar("Materials/Material_" + to_string(iSet)+"/PFF/const_ell");
 
-    } catch (const std::exception& e) {
-        cerr << "ERROR: " << e.what() << endl;
-        cerr << "Terminating!" << endl;
+    } catch (const std::exception& e){
+
+        logger.log("Exception caught in PFF material constructor:\n", "ERROR", true);
+        logger.log("    " + std::string(e.what()), "", false);
+        logger.log("    Critical error encountered. Terminating!\n", "", false);
         exit(EXIT_FAILURE);
+        
     }
 }
 
